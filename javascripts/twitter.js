bf05cad31aa44a2f41808ea72b5290c616f615a6
Ivan Mushketyk <ivan.mushketik@gmail.com>
2015-09-11 08:56:41 +0100

    Site updated at 2015-09-11 07:56:41 UTC

--- javascripts/twitter.js
+++ javascripts/twitter.js
@@ -54,7 +54,7 @@ function linkifyTweet(text, url) {
       text = text.replace(shortUrl, ">"+url[u].display_url);
     }
   }
-  return text
+  return text;
 }
 
 function showTwitterFeed(tweets, twitter_user) {
@@ -71,8 +71,8 @@ function getTwitterFeed(user, count, replies) {
   count = parseInt(count, 10);
   $.ajax({
       url: "https://api.twitter.com/1/statuses/user_timeline/" + user + ".json?trim_user=true&count=" + (count + 20) + "&include_entities=1&exclude_replies=" + (replies ? "0" : "1") + "&callback=?"
-    , dataType: 'jsonp'
+    , type: 'jsonp'
     , error: function (err) { $('#tweets li.loading').addClass('error').text("Twitter's busted"); }
     , success: function(data) { showTwitterFeed(data.slice(0, count), user); }
-  })
+  });
 }
